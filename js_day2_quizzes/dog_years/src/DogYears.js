e92de4002d0ae5cce7053adbe760a7790d915b57
sranso <sranso@gmail.com>
2014-01-21 21:12:03 -0500

    solve dog years

--- js_day2_quizzes/dog_years/src/DogYears.js
+++ js_day2_quizzes/dog_years/src/DogYears.js
@@ -2,5 +2,17 @@
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
 
 function Dog() {
-
+  this.age = Math.floor(Math.random()*15+1)
 }
+
+Dog.prototype.ageInYears = function() {
+  return this.age;
+};
+
+Dog.prototype.ageInDays = function() {
+  return this.age*365;
+};
+
+Dog.prototype.ageInDogYears = function() {
+  return this.age*7;
+};
